2a923540a7012b93743b6dfa15dfa1621a5317b3
sycod <consult@sycod.fr>
2024-11-01 15:39:00 +0100

    ONNX done

--- trained_2_onnx_infer.py
+++ trained_2_onnx_infer.py
@@ -13,32 +13,33 @@ with open("config.yaml", "r") as f:
     cfg = yaml.safe_load(f)
 APP_PATH = cfg["app_data"]["local_path"]
 MODEL = os.path.join(APP_PATH, cfg["app_data"]["model"])
+ONNX = os.path.join(APP_PATH, cfg["app_data"]["onnx"])
 BREEDS = cfg["app_data"]["breeds"]
 # logging configuration (see all outputs, even DEBUG or INFO)
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 # *****************************************************
-# # IF NOT DONE, first load .keras model for future ONNX conversion
-# logging.info("\n👉 Model path: " + MODEL)
-# model = load_model(MODEL)
-# logging.info(model.summary())
-# logging.info(model.name)
+# IF NOT DONE, first load .keras model for future ONNX conversion
+logging.info("\n👉 Model path: " + MODEL)
+model = load_model(MODEL)
+logging.info(model.summary())
+logging.info(model.name)
 
 # 🚧 remove useless data augmentation layer
 
-# # ONNX conversion with tf2onnx
-# import tf2onnx
-# import onnx
+# ONNX conversion with tf2onnx
+import tf2onnx
+import onnx
 
-# # 1. if needed, force / simulate the 'output_names' variable
-# model.output_names = ['output']
-# # 2. Specify an input signature (adjust for each model)
-# input_signature = [tf.TensorSpec(shape=[None, 224, 224, 3], dtype=tf.float32)]
-# # 3. Convert Keras model to ONNX
-# onnx_model, _ = tf2onnx.convert.from_keras(model, input_signature=input_signature)
-# # 4. Save ONNX model
-# onnx.save_model(onnx_model, 'mon_modele.onnx')
+# 1. if needed, force / simulate the 'output_names' variable
+model.output_names = ['output']
+# 2. Specify an input signature (adjust for each model)
+input_signature = [tf.TensorSpec(shape=[None, 224, 224, 3], dtype=tf.float32)]
+# 3. Convert Keras model to ONNX
+onnx_model, _ = tf2onnx.convert.from_keras(model, input_signature=input_signature)
+# 4. Save ONNX model
+onnx.save_model(onnx_model, ONNX)
 
 # *****************************************************
 # AFTER CONVERSION, test ONNX model with onnxruntime to check behaviour
@@ -50,7 +51,7 @@ import numpy as np
 # inputs
 img_path = "app_data/chow/n02112137_9159.jpg"
 img_size = (224, 224)
-session = ort.InferenceSession("mon_modele.onnx")
+session = ort.InferenceSession(ONNX)
 
 # preprocess image
 # resize
